4ed64ecdbaa6062dbc8ef776f422b4d9a6163da2
karanzhenya <63317606+karanzhenya@users.noreply.github.com>
2022-04-29 21:42:45 +0300

    rename

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,7 @@
 import {SearchField} from "./search/SearchField";
 import github_icon from "../../assets/github_icon.png"
 import s from './Header.module.scss'
-import axios from "axios";
 
-const instance = axios.create({
-    baseURL: 'http://localhost:3001',
-    withCredentials: true
-})
 
 export const Header = () => {
     return (
@@ -15,11 +10,6 @@ export const Header = () => {
                 <a href='https://github.com/'>
                     <img src={github_icon} alt='logo'/>
                 </a>
-                <button onClick={() => {
-                    instance.get('/data').then((res)=> {
-                        console.log(res.data)
-                    })
-                }}>click</button>
                 <SearchField/>
             </div>
         </div>
